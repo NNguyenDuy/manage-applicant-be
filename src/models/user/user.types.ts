648ec4e161dd2a57517218c890bec8f8a9084b58
NNguyenDuy <nguyennduy1708@gmail.com>
2024-10-04 15:26:04 +0700

    Feat: add some graphql in models

--- src/models/user/user.types.ts
+++ src/models/user/user.types.ts
@@ -1,5 +1,6 @@
+import { I_Company } from './../company/company.types'
+import { I_CandidateProfile } from './../candidate-profile/candidate-profile.types'
 import { ObjectId } from 'mongoose'
-
 export interface I_User {
   fullName?: string
   email: string
@@ -7,4 +8,6 @@ export interface I_User {
   role?: 'admin' | 'recruiter' | 'candidate'
   companyId?: ObjectId
   profileId?: ObjectId
+  candidateProfile?: I_CandidateProfile
+  company?: I_Company
 }
