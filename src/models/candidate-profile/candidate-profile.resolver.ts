04552d38fc12432ffd373d87054f9e6ecabef06c
NNguyenDuy <nguyennduy1708@gmail.com>
2024-10-11 15:37:01 +0700

    Fix

--- src/models/candidate-profile/candidate-profile.resolver.ts
+++ src/models/candidate-profile/candidate-profile.resolver.ts
@@ -1,14 +1,17 @@
-import { candidateProfileController } from './candidate-profile.controller';
-import { I_CandidateProfile } from './candidate-profile.types';
-import mongoose from 'mongoose';
+import { candidateProfileController } from './candidate-profile.controller'
+import { I_CandidateProfile } from './candidate-profile.types'
+import mongoose from 'mongoose'
 
 export const candidateProfileResolvers = {
   Query: {
     getAllCandidateProfiles: async (): Promise<I_CandidateProfile[]> => {
-      return await candidateProfileController.getAllCandidateProfiles();
+      return await candidateProfileController.getAllCandidateProfiles()
     },
-    getCandidateProfile: async (_: any, { id }: { id: string }): Promise<I_CandidateProfile | null> => {
-      return await candidateProfileController.getCandidateProfile(id);
+    getCandidateProfile: async (
+      _: any,
+      { id }: { id: string }
+    ): Promise<I_CandidateProfile | null> => {
+      return await candidateProfileController.getCandidateProfile(id)
     },
   },
   Mutation: {
@@ -16,17 +19,25 @@ export const candidateProfileResolvers = {
       _: any,
       { userId, resume }: { userId: string; resume: any }
     ): Promise<{ message: string; data: I_CandidateProfile | null }> => {
-      const objectIdUserId = new mongoose.Types.ObjectId(userId);
-      return await candidateProfileController.createCandidateProfile({ userId: objectIdUserId, resume });
+      const objectIdUserId = new mongoose.Types.ObjectId(userId)
+      return await candidateProfileController.createCandidateProfile({
+        userId: objectIdUserId,
+        resume,
+      })
     },
     updateCandidateProfile: async (
       _: any,
       { id, resume }: { id: string; resume?: any }
     ): Promise<I_CandidateProfile | null> => {
-      return await candidateProfileController.updateCandidateProfile(id, { resume });
+      return await candidateProfileController.updateCandidateProfile(id, {
+        resume,
+      })
     },
-    deleteCandidateProfile: async (_: any, { id }: { id: string }): Promise<I_CandidateProfile | null> => {
-      return await candidateProfileController.deleteCandidateProfile(id);
+    deleteCandidateProfile: async (
+      _: any,
+      { id }: { id: string }
+    ): Promise<I_CandidateProfile | null> => {
+      return await candidateProfileController.deleteCandidateProfile(id)
     },
   },
-};
+}
