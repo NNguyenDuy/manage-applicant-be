c4a8244a840f3bdd55ebd586a41e44c064fbb4eb
NguyenDuy <nguyennduy1708@gmail.com>
2024-10-17 10:31:22 +0700

    fix update job

--- src/models/job/job.controller.ts
+++ src/models/job/job.controller.ts
@@ -14,34 +14,46 @@ export const jobController = {
     return await JobModel.findOne({ _id: jobId })
   },
 
-  getAllJobs: async (idDel: boolean): Promise<IJobDocument[]> => {
-    return await JobModel.find({ idDel }); 
+  getAllJobs: async (): Promise<IJobDocument[]> => {
+    return await JobModel.find()
   },
-  getJobsWithFilters: async (Jtitle: string, Jlocation: string, JCategory: string, idDel: boolean): Promise<IJobDocument[]> => {
+  getJobsWithFilters: async (
+    Jtitle: string,
+    Jlocation: string,
+    JCategory: string,
+    idDel: boolean
+  ): Promise<IJobDocument[]> => {
     const query: any = {}
-    
+
     if (Jtitle) {
       query.title = { $regex: Jtitle, $options: 'i' }
     }
-    
+
     if (Jlocation) {
       const foundLocation = await LocationModel.findOne({ city: Jlocation })
       if (foundLocation) {
         query.locationId = foundLocation._id
       }
     }
-    
+
     if (JCategory) {
-      const foundJobCategory = await JobCategoryModel.findOne({ name: JCategory }) as IJobCategoryDocument | null;
-      if (foundJobCategory && mongoose.Types.ObjectId.isValid(foundJobCategory.name)) {
-        query.categoryId = foundJobCategory._id;
+      const foundJobCategory = (await JobCategoryModel.findOne({
+        name: JCategory,
+      })) as IJobCategoryDocument | null
+      if (
+        foundJobCategory &&
+        mongoose.Types.ObjectId.isValid(foundJobCategory.name)
+      ) {
+        query.categoryId = foundJobCategory._id
       }
     }
-    query.idDel = idDel;
-    
+    query.idDel = idDel
+
     return await JobModel.find(query)
   },
-  getMaintainJobsByCompany: async (companyId: string): Promise<IJobDocument[]> => {
+  getMaintainJobsByCompany: async (
+    companyId: string
+  ): Promise<IJobDocument[]> => {
     return await JobModel.find({ companyId, idDel: false })
   },
   updateJob: async (
@@ -52,20 +64,18 @@ export const jobController = {
       new: true,
     })
   },
-  updateIsDel: async (jobId: string, isDel: boolean): Promise<IJobDocument | null> => {
+  updateIsDel: async (
+    jobId: string,
+    isDel: boolean
+  ): Promise<IJobDocument | null> => {
     return await JobModel.findOneAndUpdate(
       { _id: jobId },
-      { idDel: isDel },  
+      { idDel: isDel },
       { new: true }
-    );
-  },
-  
-  
-  deleteJob: async (jobId: string): Promise<void> => {
-    await JobModel.updateOne({ _id: jobId }, { idDel: true })
+    )
   },
 
-  getMaintainJobsByCompany: async (companyId: string): Promise<IJobDocument[]> => {
-    return await JobModel.find({ companyId, idDel: false })
+  deleteJob: async (jobId: string): Promise<void> => {
+    await JobModel.updateOne({ _id: jobId }, { idDel: true })
   },
 }
